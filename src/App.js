9ca0b8acaa42e09c460e5dcfbbd79b420c0be38d
JUSTIKA-MAC-ENG-04 <engineering_04@JUSTIKA-MAC-ENG-04.local>
2021-04-06 10:28:02 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: null,
+      items: [],
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete(this);
+  }
+
+  handleInputChange(e) {
+    const target = e.target;
+    this.setState({ [target.name]: target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.newItem) {
+      
+    }
+    this.setState((prev) => ({
+      items: [...prev.items, prev.newItem],
+    }));
+    this.clearForm();
+  }
+
+  handleDelete(deletedItem) {
+    const newList = this.state.items.filter(item => {
+      return item != deletedItem;
+    })
+    this.setState(prev => ({
+      items: newList
+    }))
+  }
+
+  clearForm() {
+    this.setState({ newItem: null });
+    document.getElementById("form").reset();
+  }
+
+  render() {
+
+    const list = this.state.items.map(item => {
+      return <li>{item}
+        <button onClick={this.handleDelete}>delete</button>
+      </li>
+    })
+
+    return (
+      <div className="container">
+        <h1 className="title">To-Not-Do-List</h1>
+        <div className="list">
+          <ul>
+           {list}
+          </ul>
+        </div>
+        <form id="form" className="input" onSubmit={this.handleSubmit}>
+          <input
+            name="newItem"
+            type="text"
+            value={this.state.newItem}
+            onChange={this.handleInputChange}
+          />
+          <button type="submit">add</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
