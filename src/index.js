63b3dbc22df464cbf4fc8833bb3ef5fc831025cc
syafiq <abdillah.syafiq@gmail.com>
2021-04-08 12:05:37 +0800

    asd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Biodata from "./Biodata"
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Biodata from "./Biodata";
+import reportWebVitals from "./reportWebVitals";
+
+// Redux
+
+import { Provider } from "react-redux";
+import store from './redux/store.js'
 
 ReactDOM.render(
-  <Biodata />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Biodata />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
